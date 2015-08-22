44977b7daa56ff7109214ec7f577339cefe8b69e
AJP <allenatmc@gmail.com>
2015-08-22 11:27:32 -0500

    make translate button call correct function

--- german.js
+++ german.js
@@ -3,8 +3,24 @@ console.log('German JS file linked.');
 
 var Greet = (function(greet) {
 
+	var germanLexicon = {
+
+			Merry: "Frohe",
+			Christmas: "Weihnachten",
+			and: "und",
+			a: "ein",
+			Happy: "glückliches",
+			New: "neues",
+			Year: "Jahr!"
+
+	};
+
   greet.translateToGerman = function(input) {
-    console.log('Tried to Tanslate to German');
+
+  		console.log("Translating to German")
+  		console.log(input);
+  		output = document.getElementById('output');
+
   };
 
 
--- main.js
+++ main.js
@@ -14,9 +14,19 @@ var Greet = (function() {
         var inputText = document.getElementById('englishText').value;
         var language = document.getElementById('languages').value;
 
+        var wordsArray = inputText.split(" ");
+
         console.log('You need to translate ' + inputText + ' to ' + language);
 
+        console.log(wordsArray);
 
+        if (wordsArray.length > 0 && language == 'German') {
+          Greet.translateToGerman(wordsArray);
+        };
+
+        if (wordsArray.length > 0 && language == 'French') {
+          Greet.translateFrench(wordsArray);
+        }
     }
 
   };
