3932631a514f61347a4b0864d1dcd5e8b69e8bcf
Sybia Roberson <sybiaroberson@Sybias-MacBook-Air-2.local>
2015-08-22 11:50:06 -0500

    final french prototype

---
+++ french.js
@@ -0,0 +1,33 @@
+var Greet = (function(greet) {
+var frenchMessage = "";  
+    var frenchLexicon = {
+      Merry: "Joyeux",
+      Christmas: "Nöel",
+      and: "et",
+      a: "un",
+      Happy: "Content",
+      New: "Nouveau",
+      Year: "An",
+      Everyone: "Tout le monde"
+
+    }    
+     
+  greet.translateFrench = function(input) {
+    
+    frenchMessage = " ";
+
+    for(i = 0 ; i < input.length ; i++){
+
+    frenchMessage += ( frenchLexicon[input[i]] || input[i] ) + " ";
+
+  output = document.getElementById('output');
+  output.innerHTML = frenchMessage; 
+  };
+
+
+  };
+
+
+  return greet;
+
+})(Greet);
