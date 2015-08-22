a438417f84f1bc10f0cdbd79c7dc4af86353f22b
AJP <allenatmc@gmail.com>
2015-08-22 11:03:08 -0500

    add button and translate function

--- german.js
+++ german.js
@@ -1,3 +1,13 @@
 console.log('German JS file linked.');
 
 
+var Greet = (function(greet) {
+
+  greet.translateToGerman = function(input) {
+    console.log('Tried to Tanslate to German');
+  };
+
+
+  return greet;
+
+})(Greet);
--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 var Greet = (function() {
 
+
+
   return {
 
     getInput: function() {
       var inputText = document.getElementById('englishText').value;
       return inputText;
-    }
+    },
 
+    translate: function() {
+
+        var inputText = document.getElementById('englishText').value;
+        var language = document.getElementById('languages').value;
+
+        console.log('You need to translate ' + inputText + ' to ' + language);
+
+
+    }
 
   };
 })();
+
+
+document.getElementById('translate').addEventListener("click", Greet.translate);
+
